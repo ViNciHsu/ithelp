8cf744c53aec67fa449fccf20605f7f0c40a220c
ViNciHsu <ViNciHsu>
2020-10-17 00:24:33 +0800

    commit for basic views and layouts

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticlesController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::resource('articles',ArticlesController::class);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// 使用文章列表當作首頁
+Route::get('/',[ArticlesController::class,'index'])->name('root');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
